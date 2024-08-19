b346e031c1befc90ab2894bdaa9e238b7f0d06c8
ahmadmuhyiii <ahmadmuhyi434@gmail.com>
2024-08-19 23:03:43 +0700

    init

---
+++ app/albums/page.tsx
@@ -0,0 +1,23 @@
+import Card from "@/components/card";
+import Link from "next/link";
+import { getImages } from "@/lib/data";
+
+export default async function Albums() {
+    const images = await getImages();
+
+    return (
+        <div className="max-w-screen-lg mx-auto py-14">
+            <div className="flex items-end justify-between">
+                <h1 className="text-4xl font-bold"> Latest Images</h1>
+                <Link href="/create" className=" py-3 px-6 bg-blue-600 hover:bg-blue-700 text-white">
+                 New Image</Link>
+            </div>
+            <div className="grid md:grid-cols-3 gap-5 mt-10">
+            {images.map((item) => (
+                <Card key={item.id} data={item} />
+            ))}
+            
+            </div>
+        </div>
+    )
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+            protocol: "https",
+            hostname: "2nypubhterxgy7id.public.blob.vercel-storage.com"
+            },
+        ],
+    },
+
+};
 
 export default nextConfig;
